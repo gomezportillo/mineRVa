6ec2d16190d0d5a74c19ecd706f1af085941045e
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-02-25 00:12:27 +0100

    Changing scenes on collisioning doors using an script

---
+++ Assets/Scripts/DoorCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorCollider : MonoBehaviour
+{
+    public string Scene_name;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collisioning");
+        SceneManager.LoadScene(Scene_name);
+    }
+}
