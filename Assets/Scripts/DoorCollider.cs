5f253d5b87250a4eecf23c92707250652aebd38c
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-03-03 18:11:18 +0100

    Moved ground and environment to another scene and loading it from rooms

--- Assets/Scripts/DoorCollider.cs
+++ Assets/Scripts/DoorCollider.cs
@@ -5,21 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class DoorCollider : MonoBehaviour
 {
-    public string Scene_name;
+    public string scene_name;
+
+    protected string base_scene_name = "Base_room";
+
+    protected bool debug = true;
 
     void Start()
     {
-        Debug.Log("Creating door...");
-
+        MyLoadScene(base_scene_name, LoadSceneMode.Additive);
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) // Next scene
+        {
+            MyLoadScene(scene_name, LoadSceneMode.Single);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collisioning");
-        SceneManager.LoadScene(Scene_name);
+        MyLoadScene(scene_name, LoadSceneMode.Single);
     }
+
+    private void MyLoadScene(string scene_name, LoadSceneMode mode)
+    {
+        if (debug)
+        {
+            Debug.Log(message: "Loading scene " + scene_name + " in mode " + mode);
+        }
+
+        if (scene_name != "" && !SceneManager.GetSceneByName(scene_name).isLoaded)
+        {
+            SceneManager.LoadScene(scene_name, mode);
+        }
+    }
+
+
+
 }
