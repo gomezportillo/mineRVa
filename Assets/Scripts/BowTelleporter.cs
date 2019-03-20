d514ad78d09bd36ce5e569f623dbf46ecd9adc86
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-03-20 17:59:27 +0100

    Teleporting back to the table the bow if it touches the ground

---
+++ Assets/Scripts/BowTelleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowTelleporter : MonoBehaviour
+{
+    public Transform Bow;
+
+    private readonly string TAG_NAME = "Bow";
+
+    private Vector3 initial_position;
+    private Quaternion initial_rotation;
+    private Vector3 initial_velocity;
+
+    private void Awake()
+    {
+        initial_position = Bow.position;
+        initial_rotation = Bow.rotation;
+        initial_velocity = new Vector3(0, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == TAG_NAME)
+        {
+            Debug.Log("Telleporting bow...");
+            Bow.position = initial_position;
+            Bow.rotation = initial_rotation;
+            Bow.GetComponent<Rigidbody>().velocity = initial_velocity;
+        }
+    }
+}
