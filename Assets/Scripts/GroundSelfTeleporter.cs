5f43fadeeee0b2fb733d75a2ec8a23e4338793bb
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-03-31 20:38:42 +0200

    Delegating on the objects to teleport themselves back if they touch the ground and applied to all grabbable objects

---
+++ Assets/Scripts/GroundSelfTeleporter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GroundSelfTeleporter : MonoBehaviour
+{
+    public string GroundTag = "Ground";
+
+    private Vector3 initial_position;
+    private Quaternion initial_rotation;
+    private Vector3 initial_velocity;
+    private Vector3 initial_ang_velocity;
+
+    private void Awake()
+    {
+        initial_position = transform.position;
+        initial_rotation = transform.rotation;
+        initial_velocity = new Vector3(0, 0, 0);
+        initial_ang_velocity = new Vector3(0, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == GroundTag)
+        {
+            Debug.Log("Teleporting object after touching ground...");
+
+            transform.position = initial_position;
+            transform.rotation = initial_rotation;
+            transform.GetComponent<Rigidbody>().velocity = initial_velocity;
+            transform.GetComponent<Rigidbody>().angularVelocity = initial_ang_velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/GroundTeleporter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GroundTeleporter : MonoBehaviour
+{
+    public string GroundTag = "Ground";
+    public Transform instance;
+
+    private Vector3 initial_position;
+    private Quaternion initial_rotation;
+    private Vector3 initial_velocity;
+    private Vector3 initial_ang_velocity;
+
+    private void Awake()
+    {
+        initial_position = instance.position;
+        initial_rotation = instance.rotation;
+        initial_velocity = new Vector3(0, 0, 0);
+        initial_ang_velocity = new Vector3(0, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == GroundTag && instance != null)
+        {
+            Debug.Log("Teleporting object " + instance.name + " to initial position");
+
+            instance.position = initial_position;
+            instance.rotation = initial_rotation;
+            instance.GetComponent<Rigidbody>().velocity = initial_velocity;
+            instance.GetComponent<Rigidbody>().angularVelocity = initial_ang_velocity;
+        }
+    }
+}
