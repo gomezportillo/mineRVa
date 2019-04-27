f76ccb61299aa68a8209cc4fb289da424a0076b6
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-04-27 14:03:24 +0200

    Fixed room 5 drawers and added hidden interactable key and working doors

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    public Transform IgnoreWith;
+
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == IgnoreWith.name)
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
+    }
+}
