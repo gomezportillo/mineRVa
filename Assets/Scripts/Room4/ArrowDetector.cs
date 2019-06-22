f0b07385d6def0584fb6103395768ff38278055a
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-06-22 19:29:23 +0200

    Warning player when shooting guard

---
+++ Assets/Scripts/Room4/ArrowDetector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class ArrowDetector : MonoBehaviour
+{
+    public GameObject DialogManagerHolder;
+
+    private readonly string ARROW_TAG = "Arrow";
+    private DialogManager dialogManagerScript;
+   
+    private void Awake()
+    {
+        if (DialogManagerHolder)
+        {
+            dialogManagerScript = DialogManagerHolder.GetComponent<DialogManager>();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ARROW_TAG)
+        {
+            if (dialogManagerScript != null)
+            {
+                dialogManagerScript.ShowErrorDialog();
+            }
+        }
+    }
+}
