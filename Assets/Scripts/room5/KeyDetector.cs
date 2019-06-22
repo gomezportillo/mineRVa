ccfe40141ce873d0a4b9bc3694b94bc55056f2fc
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-06-22 20:06:29 +0200

    Adding room 5 dialogs and updated key detector script to use events

---
+++ Assets/Scripts/room5/KeyDetector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class KeyDetector : MonoBehaviour
+{
+    public GameObject Door;
+    public string KEY_TAG;
+
+    private DoorTeletransporter doorTeletransporterScript;
+
+    private void Start()
+    {
+        if (Door != null)
+        {
+            doorTeletransporterScript = Door.GetComponent<DoorTeletransporter>();
+            if (doorTeletransporterScript != null)
+            {
+                doorTeletransporterScript.Disable();
+            }
+            else
+            {
+                Debug.Log("Door Teletransporter Script cannot be found on object");
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += OnKeySnapped;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone -= OnKeySnapped;
+    }
+
+    internal void OnKeySnapped(object sender, SnapDropZoneEventArgs e)
+    {
+        if (e.snappedObject.tag == KEY_TAG)
+        {
+            if (doorTeletransporterScript != null)
+            {
+                doorTeletransporterScript.Enable();
+            }
+        }
+    }
+}
