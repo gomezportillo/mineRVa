bccaba5c5d35d9c40f3b7494c9d9d9ced8843ebc
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-06-23 21:25:24 +0200

    Finished menu. Playing, showing credtis and exiting

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Button playButton;
+    public Button creditsButton;
+    public Button creditsBackButton;
+    public Button exitButton;
+
+    public GameObject menuBackground;
+    public GameObject creditsBackground;
+
+    private string FIRST_SCENE = "room0";
+
+    private void Start()
+    {
+        if (playButton != null)
+        {
+            playButton.onClick.AddListener(OnClickPlay);
+        }
+        if (creditsButton != null)
+        {
+            creditsButton.onClick.AddListener(OnClickCredits);
+        }
+        if (creditsBackButton != null)
+        {
+            creditsBackButton.onClick.AddListener(OnClickCreditsBack);
+        }
+        if (exitButton != null)
+        {
+            exitButton.onClick.AddListener(OnClickExit);
+        }
+        
+    }
+
+    private void OnClickPlay()
+    {
+        SceneManager.LoadScene(FIRST_SCENE, LoadSceneMode.Single);
+    }
+
+    private void OnClickCredits()
+    {
+        menuBackground.SetActive(false);
+        creditsBackground.SetActive(true);
+    }
+
+    private void OnClickCreditsBack()
+    {
+        creditsBackground.SetActive(false);
+        menuBackground.SetActive(true);
+    }
+
+    private void OnClickExit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
